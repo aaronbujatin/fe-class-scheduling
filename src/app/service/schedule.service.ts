8d227e66aabe494109a5a9fa1b34df2978dd6064
Aaron Bujatin <aaronbujatin@gmail.com>
2023-12-02 07:07:32 +0800

    Add files via upload

--- src/app/service/schedule.service.ts
+++ src/app/service/schedule.service.ts
@@ -7,13 +7,13 @@ import { environment } from 'src/environments/environment.development';
 })
 export class ScheduleService {
 
-  constructor(private httpClient : HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
   private readonly LOCAL_API = "http://localhost:8080";
-  private readonly DEPLOY_API =  environment.apiUrl;
+  private readonly DEPLOY_API = environment.apiUrl;
 
-  public getScheduleByInstructorNameAndCourse(instructorName : string, course : string){
-    return this.httpClient.get(`${this.LOCAL_API}/api/v1/schedules?instructorName=${instructorName}&course=${course}`)
+  public getScheduleByInstructorNameAndCourse(instructorName: string, course: string) {
+    return this.httpClient.get(`${this.DEPLOY_API}/api/v1/schedules?instructorName=${instructorName}&course=${course}`)
   }
 
 }
